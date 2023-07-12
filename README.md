8f80cc5f346af6f8768ea72a0d531c032622b370
sophiecookie <104461485+sophiecookie@users.noreply.github.com>
2023-07-12 15:43:03 -0700

    created the sketch of the game with JS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+This is a simple "rock paper scissors" game created with JavaScript by a newbie.
---
+++ script.js
@@ -0,0 +1,65 @@
+function getComputerChoice(){
+    const computerChoice = ["rock", "paper", "scissors"];
+    const computerRandomChoice = computerChoice[Math.floor(Math.random()*3)];
+    return computerRandomChoice;
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+        if(playerSelection == "rock" && computerSelection == "rock") {
+            return "Tie! Rock <=> Rock";
+        } else if (playerSelection == "rock" && computerSelection == "paper"){
+            computerScore++;
+            return "You Lose! Rock <=> Paper."
+            
+        } else if (playerSelection == "rock" && computerSelection == "scissors"){
+            playerScore++;
+            return "You Win! Rock <=> Scissors."
+
+        } else if (playerSelection == "paper" && computerSelection == "rock"){
+            playerScore++;
+            return "You Win! Paper <=> Rock."
+
+        } else if (playerSelection == "paper" && computerSelection == "paper"){
+            return "Tie! Paper <=> Paper";
+
+        } else if (playerSelection == "paper" && computerSelection == "scissors"){
+            computerScore++;
+            return "You Lose! Paper <=> Scissors."
+
+        } else if (playerSelection == "scissors" && computerSelection == "rock"){
+            computerScore++;
+            return "You Lose! Scissors <=> Rock."
+
+        } else if (playerSelection == "scissors" && computerSelection == "paper"){
+            playerScore++;
+            return "You Win! Scissors <=> Paper."
+
+        } else if (playerSelection == "scissors" && computerSelection == "scissors"){
+            return "Tie! Scissors <=> Scissors";
+
+        } else {
+            return "Please enter correct option."
+        }
+}
+
+var computerScore = 0;
+var playerScore = 0;
+
+function game(){
+    for (var i = 0; i < 5; i++){
+        const playerSelection = prompt("What's your move?");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (computerScore > playerScore){
+        return "Computer Wins!"
+    } else if (playerScore > computerScore) {
+        return "Player Wins!"
+    } else {
+        return "It's a Tie!"
+    }
+}
+
+game();
